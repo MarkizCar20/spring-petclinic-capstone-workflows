a1f0ec9a0f42b59a4077b9a718c0f1b15bbaabbc
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2024-02-07 16:11:39 +0100

    add zone to demo instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ provider "google" {
 resource "google_compute_instance" "demo-instance" {
   name = "demo-instance"
   machine_type = "f1-micro"
+  zone = "us-central1-c"
 
   boot_disk {
     initialize_params {
