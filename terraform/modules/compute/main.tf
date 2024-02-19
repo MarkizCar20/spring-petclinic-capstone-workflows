df27322735c4937d6214d9246c6965285d54d3f0
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2024-02-19 13:41:37 +0100

    replace unmanaged instace group with managed instance group

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,36 +1,51 @@
+resource "google_compute_instance_template" "group_instance" {
+  name = "instance-template"
+
+  machine_type = "e1-micro"
+  disk {
+    source_image = "debian-cloud/debian-10"
+  }
+
+  network_interface {
+    network = "default"
+    #Access config for ephemeral public IP If needed
+  }
+
+  metadata_startup_script = file("./modules/compute/web1.sh")
+}
+
 #INSTANCE GROUP 1
-resource "google_compute_instance_group" "webservers" {
+resource "google_compute_instance_group_manager" "webservers" {
   name = "terraform-webservers-us"
+  base_instance_name = "instance_x"
   description = "terraform test instance group"
   zone = "us-central1-c"
 
-  instances = [
-    google_compute_instance.default.id
-  ]
+  target_size = 1
+
+  version {
+    instance_template = google_compute_instance_template.group_instance.id
+  }
+
   named_port {
     name = "http"
     port = "80"
   }
 }
 
-resource "google_compute_instance" "default" {
-  name = "web1"
-  machine_type = "f1-micro"
-  zone = "us-central1-c"
+resource "google_compute_autoscaler" "group_autoscaler" {
+  name = "group-autoscaler"
+  target = google_compute_instance_group_manager.webservers.self_link
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
+  autoscaling_policy {
+    max_replicas = 10
+    min_replicas = 1
+    cooldown_period = 60
+    
+    cpu_utilization {
+      target = 0.5
     }
   }
-
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-  metadata_startup_script = file("./modules/compute/web1.sh")
 }
 
 #to allow http traffic to the instances
