319470756660a6e6caad60c78145291cee30b65c
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2024-02-19 14:53:50 +0100

    try to fix startup script

--- terraform/modules/compute/web1.sh
+++ terraform/modules/compute/web1.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-sleep 30
 sudo apt-get update
 sudo apt-get install apache2 -y
 sudo a2ensite default-ssl
 sudo a2enmod ssl
-cd /var/www/html
+sudo cd /var/www/html
 sudo mkdir app1
 sudo echo "Application 1" | \
 tee /var/www/html/app1/index.html
