af89165c6fa602734d65b28ac034bcf8281434b2
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2024-02-21 14:41:41 +0100

    remove instance ip get from terraform

--- terraform/modules/compute/host_instances/outputs.tf
+++ terraform/modules/compute/host_instances/outputs.tf
@@ -2,7 +2,3 @@ output "compute_instance_group" {
   value = google_compute_instance_group_manager.webservers.instance_group
 }
 
-output "instance_ips" {
-  value = [for instance_name in data.google_compute_instance_group_manager.webservers_info.instances : google_compute_instance.instance[instance_name].network_interface.0.access_config[0].nat_ip]
-}
-
--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,7 +1,3 @@
 output "compute_instance_group" {
   value = module.host_instances.compute_instance_group
 }
-
-output "instance_ips" {
-  value = module.host_instances.instance_ips
-}
