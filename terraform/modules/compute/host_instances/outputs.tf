3d024d0632b5082d8c31a10d7009779b9628decd
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2024-02-21 14:09:28 +0100

    fix instance ip addressing

--- terraform/modules/compute/host_instances/outputs.tf
+++ terraform/modules/compute/host_instances/outputs.tf
@@ -3,5 +3,5 @@ output "compute_instance_group" {
 }
 
 output "instance_ips" {
-  value = google_compute_instance_group_manager.webservers.instance_group[0].instances[*].network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance_group_manager.webservers.instances[*].network_interface[0].access_config[0].nat_ip
 }
