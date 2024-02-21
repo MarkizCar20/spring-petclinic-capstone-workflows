c560a47ba1fe6e7e4941a1e77965652f36a93b46
Marko Mijailovic <mijailovicmarko2000@gmail.com>
2024-02-21 14:14:02 +0100

    try to fix the instance addressing

--- terraform/modules/compute/host_instances/outputs.tf
+++ terraform/modules/compute/host_instances/outputs.tf
@@ -3,5 +3,6 @@ output "compute_instance_group" {
 }
 
 output "instance_ips" {
-  value = google_compute_instance_group_manager.webservers.instance_template.*.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance_group_manager.webservers.instance_group[0].instances[*].network_interface[0].access_config[0].nat_ip
 }
+
